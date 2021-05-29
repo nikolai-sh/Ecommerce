98f3c2786463c9b40a1323ec61a0b9637811d4e1
Nikolay <nikolain073@gmail.com>
2021-05-29 20:00:29 +0300

    Update views and home.html( test OK)

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
-from annoying.decorators import render_to
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 from .models import Item
 
-# @render_to('store/home.html')
-# def home_view(request):
-#     items = Item.objects.all()
-#     return {'items': items}
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = Item
     template_name = 'store/home.html'
