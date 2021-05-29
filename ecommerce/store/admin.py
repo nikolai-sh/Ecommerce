4d1c614e9d7906f86ac99f38398567f294e1de95
Nikolay <nikolain073@gmail.com>
2021-05-29 14:57:50 +0300

    Add models to admin

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Item, Employee, Sale
 
-# Register your models here.
+admin.site.register(Item)
+admin.site.register(Employee)
+admin.site.register(Sale)
