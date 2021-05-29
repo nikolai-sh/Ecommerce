611ff8438760d104422098dae2709158a2185878
Nikolay <nikolain073@gmail.com>
2021-05-29 14:38:04 +0300

    Create models for our store app

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.admin.models import User
 
-# Create your models here.
+
+class Item(models.Model):
+
+    title = models.CharField(max_length=255, verbose_name="Товар")
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to="images/")
+    price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Цена")
+    employee = models.ForeignKey('Employee', verbose_name="Продавец", on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title
+    
+
+class Employee(models.Model):
+
+    name = models.OneToOneField(User, verbose_name="Продавец", on_delete=models.CASCADE)
+    email = models.EmailField(max_length=200)
+
+    def __str__(self) -> str:
+        return f'{self.user.first_name} {self.user.last_name}'
+
+class Sale(models.Model):
+
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Цена")
+    qty = models.PositiveIntegerField(default=0, verbose_name="Количество")
+    date_sales = models.DateTimeField(verbose_name="Дата продажи", auto_now_add=True)
