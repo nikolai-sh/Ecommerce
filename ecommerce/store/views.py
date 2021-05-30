6abf49256007fc0c0dfc99cc0eafa151af89f253
Nikolay <nikolain073@gmail.com>
2021-05-30 20:56:43 +0300

    Create item-detail view class

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Item
 
 
 class HomePageView(ListView):
     model = Item
     template_name = 'store/home.html'
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = 'store/item_detail.html'
