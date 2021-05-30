2a68de531c9196b8beae73ca855b96700ddd703b
Nikolay <nikolain073@gmail.com>
2021-05-30 18:53:56 +0300

    Rewrite models

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,31 +1,30 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 
 class Item(models.Model):
 
-    title = models.CharField(max_length=255, verbose_name="Товар")
+    title = models.CharField(max_length=255, verbose_name="Наименование")
     slug = models.SlugField(unique=True)
-    image = models.ImageField(upload_to="images/")
+    image = models.ImageField(upload_to="media/images/")
     price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Цена")
     employee = models.ForeignKey('Employee', verbose_name="Продавец", on_delete=models.CASCADE)
-
+   
     def __str__(self) -> str:
         return self.title
     
 
 class Employee(models.Model):
 
-    name = models.OneToOneField(User, verbose_name="Продавец", on_delete=models.CASCADE)
-    email = models.EmailField(max_length=200)
-
+    name = models.CharField(max_length=255, verbose_name="Продавец")
+    
     def __str__(self) -> str:
         return f'{self.name}'
 
+
 class Sale(models.Model):
 
-    item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Цена")
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, verbose_name="Товар")
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, verbose_name="Продавец")
+    total_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Общая цена")
     qty = models.PositiveIntegerField(default=0, verbose_name="Количество")
     date_sales = models.DateTimeField(verbose_name="Дата продажи", auto_now_add=True)
