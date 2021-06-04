6c36808633bdbb48297cc1f6491a15ced5dde95a
Nikolay <nikolain073@gmail.com>
2021-06-04 19:14:42 +0300

    Create post method in ItemDetailview

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,26 +1,34 @@
-from django.http import request
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.views.generic import ListView, DetailView
-from .models import Item, Employee, Sale
+from .models import Item, Employee
+from django.views.generic.edit import FormMixin
+from .forms import SalesForms
 
 
 class HomePageView(ListView):
     model = Item
     template_name = 'store/home.html'
 
-class ItemDetailView(DetailView):
+class ItemDetailView(FormMixin, DetailView):
     model = Item
+    form_class = SalesForms
     template_name = 'store/item_detail.html'
 
+
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+        context = super(ItemDetailView, self).get_context_data(**kwargs)
         employee = Employee.objects.all()
         context["employee"] = employee
+        context["form"] = SalesForms(initial={'item': self.object})
         return context
-    
-    def post(self, request, *args, **kwargs):
-        if request.method == 'POST':
-            
-            print(employee=request.POST.get(form.option.value))
 
-            
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()     
+        if form.is_valid():        
+            form.save()
+            return redirect('home')
+
+ 
+
+ 
+                    
