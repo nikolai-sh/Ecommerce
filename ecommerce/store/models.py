ef7dcb5ed75b835fd220ad6a10e29e4571e0e7f4
Nikolay <nikolain073@gmail.com>
2021-06-04 19:13:15 +0300

    Update save models

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,7 +1,6 @@
 from django.db import models
-from django.db.models.deletion import CASCADE
 from django.urls import reverse
-from django.contrib.auth.models import User
+
 
 class Item(models.Model):
 
@@ -26,17 +25,17 @@ class Employee(models.Model):
 
 
 class Sale(models.Model):
-    customer = models.OneToOneField(User, on_delete=CASCADE, verbose_name="Покупатель", blank=True, null=True) 
+    
     item = models.ForeignKey(Item, on_delete=models.CASCADE, verbose_name="Товар")
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE, verbose_name="Продавец")
-    qty = models.PositiveIntegerField(default=0, verbose_name="Количество")
+    qty = models.PositiveIntegerField(default=1, verbose_name="Количество")
     date_sales = models.DateTimeField(verbose_name="Дата продажи", auto_now_add=True)
     total_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Общая цена", blank=True, null=True)
-    
-    def count_total_price(self):
-        """ Count total priice for item """
-        total_price = self.item.price * self.qty
-        total_price.save()      
-    
+   
+     
+    def save(self, *args, **kwargs):
+        self.total_price = self.item.price * self.qty
+        super(Sale, self).save(*args, **kwargs)
+
     def __str__(self) -> str:
         return f'Продажа №{self.id}'
