5a1a2916e700d0ede226743f3b4fde5a15d03d6c
Nikolay <nikolain073@gmail.com>
2021-06-09 20:15:40 +0300

    Create Sales list

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView, ItemDetailView
+from .views import HomePageView, ItemDetailView, SaleList
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('<slug>/', ItemDetailView.as_view(), name='item-detail')
+    path('sales/', SaleList.as_view(), name='sales-list'),
+    path('<slug>/', ItemDetailView.as_view(), name='item-detail'),
 ]
