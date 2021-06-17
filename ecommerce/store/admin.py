638903dd06bbc8e5d62df4ca52104c3d13cc99ff
Nikolay <nikolain073@gmail.com>
2021-06-18 00:33:41 +0300

    Create UpdateItemPrice model and function to create it

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Item, Employee, Sale
+from .models import Item, Employee, Sale, UpdatedItemPrice
 
 admin.site.register(Item)
 admin.site.register(Employee)
 admin.site.register(Sale)
+admin.site.register(UpdatedItemPrice)
--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.urls import reverse
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Item(models.Model):
 
     title = models.CharField(max_length=255, verbose_name="Наименование")
     slug = models.SlugField(unique=True)
-    image = models.ImageField(upload_to="images/")
+    image = models.ImageField(upload_to="images/", verbose_name="Изображение")
     price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Цена")
    
     def __str__(self) -> str:
@@ -32,8 +34,7 @@ class Sale(models.Model):
     total_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Общая цена", blank=True, null=True)
    
     class Meta:
-        ordering = ["-date_sales"]     
-    
+        ordering = ["-date_sales"]       
 
     def save(self, *args, **kwargs):
         self.total_price = self.item.price * self.qty
@@ -41,3 +42,24 @@ class Sale(models.Model):
 
     def __str__(self) -> str:
         return f'Продажа №{self.id}'
+
+class UpdatedItemPrice(models.Model):
+
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, verbose_name="Товар")
+    update_date = models.DateTimeField(verbose_name="Дата обновления цены", auto_now_add=True)
+    updated_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Новая цена", blank=True, null=True)
+    
+    class Meta:
+            ordering = ["-update_date"] 
+    
+    def __str__(self) -> str:
+        return f'Новая цена для {self.item}'
+
+
+@receiver(post_save, sender=Item)
+def create_update_item_price(sender, instance, created, **kwargs):
+    if not created:
+        UpdatedItemPrice.objects.create(item=instance, updated_price=instance.price)
+   
+        
+        
