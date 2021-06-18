ff02b0bff814ad3fca1b1fb44c61c11dafb70f60
Nikolay <nikolain073@gmail.com>
2021-06-18 10:37:15 +0300

    Update messages in user register function

--- ecommerce/user/views.py
+++ ecommerce/user/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
@@ -10,7 +9,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Ваш аккаунт зарегестрирован! Осуществите вход.')
+            messages.success(request, f'Ваш аккаунт зарегестрирован {username}!')
             return redirect('login')
     else:
         form = UserRegisterForm()
