11b52996fe4619de09da9d503c0d52e5f2501664
Nikolay <nikolain073@gmail.com>
2021-06-19 20:07:03 +0300

    Add login to register view func, and remove register from navbar to login page

--- ecommerce/user/views.py
+++ ecommerce/user/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from .forms import UserRegisterForm
 
@@ -7,10 +8,13 @@ def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
+            user = form.save()
+            username = form.cleaned_data.get('username')         
+            login(request, user)
             messages.success(request, f'Ваш аккаунт зарегестрирован {username}!')
-            return redirect('login')
+            return redirect('home')
+        else:
+            messages.info(request, f'Регистрация не удалась! Неверные данные!')
     else:
         form = UserRegisterForm()
     return render(request, 'user/register.html', {'form': form})
